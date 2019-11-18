758bdeb4c6dd8950e46bfabc4ff69f6de299b719
Lawrence Tan <lawtjh@gmail.com>
2019-11-18 22:06:09 +0800

    Add Nice UI

--- lib/UI/views/product_list_view.dart
+++ lib/UI/views/product_list_view.dart
@@ -9,31 +9,93 @@ class ProductListView extends StatelessWidget {
     return BaseView<ProductListModel>(
       onModelReady: (model) => model.getProducts(),
       builder: (context, model, child) => Scaffold(
-        body: model.state == ViewState.Busy
-            ? Center(child: CircularProgressIndicator())
-            : GridView.count(
-                crossAxisCount: 2,
-                padding: EdgeInsets.all(8.0),
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 5.0,
-                children: model.products
-                    .map((product) => Card(
-                          child: Center(
-                            child: Column(
-                              children: <Widget>[
-                                Image.network(product.imageUrl),
-                                Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: Text(product.name),
+          backgroundColor: Colors.grey.shade50,
+          appBar: AppBar(
+            title: Text("ShopNBuy"),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+              )
+            ],
+          ),
+          body: model.state == ViewState.Busy
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: <Widget>[
+                        Padding(
+                          child: Card(
+                            elevation: 4.0,
+                            margin: new EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 6.0),
+                            child: Container(
+                              height: 150.0,
+                              decoration: BoxDecoration(color: Colors.white),
+                              child: ListTile(
+                                title: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(
+                                            model.products[index].name,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 20.0,
+                                            ),
+                                          ),
+                                          Row(
+                                            children: <Widget>[
+                                              Text(
+                                                '\$${model.products[index].price.toString()}.00',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.w300,
+                                                  fontSize: 18.0,
+                                                ),
+                                              ),
+                                              IconButton(
+                                                icon: Icon(
+                                                    Icons.add_shopping_cart),
+                                                color: Colors.grey,
+                                                onPressed: () {},
+                                              )
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Colors.grey.shade100,
+                                      width: 2.0,
+                                      height: 130.0,
+                                    ),
+                                    Image.network(
+                                      model.products[index].imageUrl,
+                                      height: 100.0,
+                                      width: 100.0,
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ],
                                 ),
-                                Text(product.price.toString()),
-                              ],
+                              ),
                             ),
                           ),
-                        ))
-                    .toList(),
-              ),
-      ),
+                          padding: EdgeInsets.all(10.0),
+                        ),
+                      ],
+                    );
+                  },
+                  itemCount: model.products.length,
+                )),
     );
   }
 }
