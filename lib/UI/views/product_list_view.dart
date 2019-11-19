033403273c7d07652595d85e831f97a448a21369
Lawrence Tan <lawtjh@gmail.com>
2019-11-19 21:13:48 +0800

    Add To Cart

--- lib/UI/views/product_list_view.dart
+++ lib/UI/views/product_list_view.dart
@@ -13,10 +13,37 @@ class ProductListView extends StatelessWidget {
           appBar: AppBar(
             title: Text("ShopNBuy"),
             actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
-              )
+              new Stack(
+                children: <Widget>[
+                  new IconButton(
+                      icon: Icon(Icons.shopping_cart), onPressed: () {}),
+                  model.cartSize != 0
+                      ? new Positioned(
+                          right: 11,
+                          top: 7,
+                          child: new Container(
+                            padding: EdgeInsets.all(2),
+                            decoration: new BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            constraints: BoxConstraints(
+                              minWidth: 14,
+                              minHeight: 14,
+                            ),
+                            child: Text(
+                              '${model.cartSize}',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 8,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        )
+                      : new Container()
+                ],
+              ),
             ],
           ),
           body: model.state == ViewState.Busy
@@ -38,39 +65,51 @@ class ProductListView extends StatelessWidget {
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceBetween,
                                   children: <Widget>[
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            model.products[index].name,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w700,
-                                              fontSize: 20.0,
-                                            ),
-                                          ),
-                                          Row(
+                                    Expanded(
+                                      flex: 1,
+                                      child: InkWell(
+                                        onTap: () {
+                                          model
+                                              .addToCart(model.products[index]);
+                                        },
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 8.0),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceEvenly,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
                                             children: <Widget>[
                                               Text(
-                                                '\$${model.products[index].price.toString()}.00',
+                                                model.products[index].name,
+                                                maxLines: 2,
                                                 style: TextStyle(
-                                                  fontWeight: FontWeight.w300,
-                                                  fontSize: 18.0,
+                                                  fontWeight: FontWeight.w700,
+                                                  fontSize: 20.0,
                                                 ),
                                               ),
-                                              IconButton(
-                                                icon: Icon(
-                                                    Icons.add_shopping_cart),
-                                                color: Colors.grey,
-                                                onPressed: () {},
-                                              )
+                                              Row(
+                                                children: <Widget>[
+                                                  Text(
+                                                    '\$${model.products[index].price.toString()}.00',
+                                                    style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.w300,
+                                                      fontSize: 18.0,
+                                                    ),
+                                                  ),
+                                                  IconButton(
+                                                    icon: Icon(Icons
+                                                        .add_shopping_cart),
+                                                    color: Colors.grey,
+                                                    onPressed: () {},
+                                                  )
+                                                ],
+                                              ),
                                             ],
                                           ),
-                                        ],
+                                        ),
                                       ),
                                     ),
                                     Container(
@@ -80,9 +119,9 @@ class ProductListView extends StatelessWidget {
                                     ),
                                     Image.network(
                                       model.products[index].imageUrl,
-                                      height: 100.0,
-                                      width: 100.0,
-                                      fit: BoxFit.fill,
+                                      height: 125.0,
+                                      width: 135.0,
+                                      fit: BoxFit.fitHeight,
                                     ),
                                   ],
                                 ),
